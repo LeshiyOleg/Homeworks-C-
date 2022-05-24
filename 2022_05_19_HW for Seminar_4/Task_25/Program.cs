8059b91880bf8346c7821ceb3940092fa4d1cbb0
Oleg S <leshiyoleg@mail.ru>
2022-05-24 18:21:29 +0300

    Added Task_27 solution

---
+++ 2022_05_19_HW for Seminar_4/Task_25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// ```
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// ```
+
+double FindExpResult(int num, int exp)
+{
+    double res = 1;
+    if (exp >= 0)
+    {
+        for (int i = 1; i <= exp; i++)
+        {
+            res = res * num;
+        }
+        return res;
+    }
+    else
+    {
+        for (int i = 1; i <= Math.Abs(exp); i++)
+        {
+            res = res / num;
+        }
+        return res;
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int exponent = Convert.ToInt32(Console.ReadLine());
+
+double result = FindExpResult(number, exponent);
+Console.WriteLine($"Результат возведения числа {number} в степень {exponent}: {Math.Round(result, 4)}");
---
+++ 2022_05_19_HW for Seminar_4/Task_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// ```
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// ```
+
+int SumOfDigitsInNumber (int num)
+{
+    int result = 0;
+    while (num>0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+int digitsSum = SumOfDigitsInNumber (number);
+Console.WriteLine($"Сумма цифр в числе: {digitsSum}");
