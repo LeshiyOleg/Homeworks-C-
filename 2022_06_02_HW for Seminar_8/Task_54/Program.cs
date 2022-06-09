4756894c4a0ec3255fdf742d798c3be024048934
Oleg S <leshiyoleg@mail.ru>
2022-06-09 19:56:39 +0300

    Added hometas for Seminar 8

---
+++ 2022_06_02_HW for Seminar_8/Task_54/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] Get2DArray(int raws, int columns, int deviation)
+{
+    int[,] array2D = new int[raws, columns];
+    for (int i = 0; i < raws; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array2D[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array2D;
+}
+
+void Print2DArray(int[,] arrayToPrint)
+{
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortStringsMaxToMin(int[,] arrayToSort)
+{
+    for (int i = 0; i < arrayToSort.GetLength(0) - 1; i++)
+    {
+        for (int j = 0; j < arrayToSort.GetLength(0) - 1; j++)
+        {
+            if (arrayToSort[i, j] < arrayToSort[i, j + 1])
+            {
+                int temp = arrayToSort[i, j];
+                arrayToSort[i, j] = arrayToSort[i, j + 1];
+                arrayToSort[i, j + 1] = temp;
+            }
+        }
+    }
+    return arrayToSort;
+}
+
+
+
+Console.Write("Введите количество строк массива: ");
+int raws = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2D = Get2DArray(raws, columns, 9);
+Console.WriteLine("Заданный массив: ");
+Print2DArray(array2D);
+Console.WriteLine();
+
+int[,] sortedArray = SortStringsMaxToMin(array2D);
+Print2DArray(sortedArray);
+
